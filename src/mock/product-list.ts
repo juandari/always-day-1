73d2bb22f57cd5dc77a9c57330cc01bbdb87626d
juandari <juandaari70@gmail.com>
2025-03-13 15:46:38 +0700

    feat: add mock data

---
+++ src/mock/product-list.ts
@@ -0,0 +1,60 @@
+export const productList = {
+  ingredients: [
+    {
+      name: "Tepung",
+      price: "Rp13.200",
+      link: "https://www.tokopedia.com/find/tepung",
+      image:
+          "https://images.tokopedia.net/img/cache/300-square/hDjmkQ/2025/1/9/16a34564-0058-4c90-9763-c3707329abe5.jpg",
+        shopName: "KedaiMart Official Store",
+        rating: "4.7",
+      },
+      {
+        name: "Minyak Goreng",
+        price: "Rp24.900",
+        link: "https://www.tokopedia.com/find/minyak-goreng",
+        image:
+          "https://images.tokopedia.net/img/cache/300-square/VqbcmM/2024/9/17/8dae7f60-551c-4e96-83d1-e8e70897944a.jpg",
+        shopName: "Zeero Bakery",
+        rating: "4.7",
+      },
+      {
+        name: "Bawang Merah",
+        price: "Rp33.600",
+        link: "https://www.tokopedia.com/find/bawang",
+        image:
+          "https://images.tokopedia.net/img/cache/300-square/hDjmkQ/2023/1/11/0f1b8485-7b7b-4e32-a6d8-9a8db4b0222a.jpg",
+        shopName: "STP Bumbu",
+        rating: "4.7",
+      },
+    ],
+    tools: [
+      {
+        name: "Panci",
+        price: "Rp13.200",
+        link: "https://www.tokopedia.com/find/panci",
+        image:
+          "https://images.tokopedia.net/img/cache/300-square/VqbcmM/2024/12/21/43db64d8-e7f7-47d8-a860-863728cf4868.jpg",
+        shopName: "STP Bumbu",
+        rating: "4.7",
+      },
+      {
+        name: "Pisau Dapur",
+        price: "Rp130.200",
+        link: "https://www.tokopedia.com/find/pisau",
+        image:
+          "https://images.tokopedia.net/img/cache/300-square/VqbcmM/2024/2/2/8bbed98b-c4ad-436c-b1a7-665050a5d2af.jpg",
+        shopName: "IKEA LINDRIG",
+        rating: "4.8",
+      },
+      {
+        name: "Kulkas",
+        price: "Rp3.000.000",
+        link: "https://www.tokopedia.com/find/kulkas",
+        image:
+          "https://images.tokopedia.net/img/cache/300-square/VqbcmM/2024/4/6/f1ee572f-1f9d-4493-9856-12674817d36f.jpg",
+      shopName: "MyHartono",
+      rating: "5.0",
+    },
+  ],
+};

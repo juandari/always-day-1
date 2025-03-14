de92cc4c961ea334849fc8899a1cb48d55f9127a
Ryan Purnama <31387167+RyanPurnama29@users.noreply.github.com>
2025-03-14 15:40:31 +0700

    feat: enhance CookingInstructions component with mock data and JSON parsing (#3)
    Co-authored-by: Ryan Wijalaksana <ryan.wijalaksana@bytedance.com>

--- src/context/recipe.tsx
+++ src/context/recipe.tsx
@@ -1,31 +1,36 @@
 import { createContext, useContext, useState } from "react";
 
 export const RecipeContext = createContext({
-    dish_name : '', 
-    match_percentage : 0,
-    setData : (data: {
-        dish_name: string
-        match_percentage: number
-    }) => {}
-})
+  dish_name: "",
+	inggredients: "",
+  match_percentage: 0,
+	setIngredients: (ing: string) => {},
+  setData: (data: { dish_name: string; match_percentage: number }) => {},
+});
 
 export const useRecipe = () => {
-    return useContext(RecipeContext);
-}
+  return useContext(RecipeContext);
+};
 
+export const RecipeProvider = ({ children }) => {
+  const [dish_name, setDish_name] = useState("");
+  const [match_percentage, setMatch_percentage] = useState(0);
+	const [inggredients, setIngredients] = useState("");
 
-export const RecipeProvider = ({children}) => {
-    const [dish_name, setDish_name] = useState('');
-    const [match_percentage, setMatch_percentage] = useState(0);
-
-    return <RecipeContext.Provider value={{
-        dish_name, 
-        match_percentage, 
+  return (
+    <RecipeContext.Provider
+      value={{
+        dish_name,
+        match_percentage,
+				inggredients,
+				setIngredients,
         setData: (data) => {
-            setDish_name(data.dish_name);
-            setMatch_percentage(data.match_percentage);
-        }
-    }}>
-        {children}
+          setDish_name(data.dish_name);
+          setMatch_percentage(data.match_percentage);
+        },
+      }}
+    >
+      {children}
     </RecipeContext.Provider>
-}
+  );
+};
